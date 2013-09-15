d57aa39064bc445272655f141b882002cdd680a7
Soumendra Daas <daas.soumendra@gmail.com>
2013-09-15 10:46:48 +0530

    Added TestNG unit test for Hello.java

---
+++ src/test/java/com/daasworld/webapp/HelloTest.java
@@ -0,0 +1,19 @@
+package com.daasworld.webapp;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * User: sdaas
+ * Date: 9/15/13
+ * Time: 10:43 AM
+ */
+public class HelloTest {
+
+    @Test
+    public void testSayHello() throws Exception {
+
+        Hello h = new Hello();
+        Assert.assertEquals( h.sayHello(), "Hello World", "got incorrect response");
+    }
+}
